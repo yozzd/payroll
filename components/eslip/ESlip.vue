9d0b84c3558f0d724e0a23f7b2a8a6644d269cbf
yozzd <yozzdharma@gmail.com>
2020-09-23 15:34:26 +0700

    eslip component

---
+++ components/eslip/ESlip.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    ESLip
+  </div>
+</template>
---
+++ pages/eslip/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ESlip />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'layout1',
+  head() {
+    return {
+      title: 'E-Slip',
+    };
+  },
+  middleware: ['auth'],
+};
+</script>
