12ba76bb5b35d55829b8274d62dab5b56c8a2463
yozzd <yozzdharma@gmail.com>
2020-09-23 11:23:39 +0700

    brand component

---
+++ components/Brand.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-2xl font-black tracking-tighter">
+    Payroll
+  </div>
+</template>
--- components/Landing.vue
+++ components/Landing.vue
@@ -3,3 +3,30 @@
     Landing
   </div>
 </template>
+<template>
+  <div class="flex min-h-full">
+    <div class="w-2/5 flex flex-col p-12">
+      <div class="flex">
+        <Brand />
+      </div>
+      <div class="flex items-center h-full">
+        <div class="flex-1 flex-col space-y-8 px-12">
+          Form
+        </div>
+      </div>
+      <div class="flex">
+        <div class="flex-1">
+          &copy; 2020
+        </div>
+        <div>
+          DB
+        </div>
+      </div>
+    </div>
+    <div class="w-3/5 bg-gray-100">
+      <div class="flex justify-center items-center h-full">
+        Placeholder
+      </div>
+    </div>
+  </div>
+</template>
