a90b59feb3bbc5cd370dbaef89f2bef61bf0cddd
yozzd <yozzdharma@gmail.com>
2020-09-23 11:58:53 +0700

    login form

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,9 +1,4 @@
 <template>
-  <div>
-    Landing
-  </div>
-</template>
-<template>
   <div class="flex min-h-full">
     <div class="w-2/5 flex flex-col p-12">
       <div class="flex">
@@ -11,7 +6,25 @@
       </div>
       <div class="flex items-center h-full">
         <div class="flex-1 flex-col space-y-8 px-12">
-          Form
+          <el-form
+            ref="form"
+            :model="form"
+            :rules="rules"
+            :hide-required-asterisk="true"
+            label-position="top"
+          >
+            <el-form-item label="Username" prop="username">
+              <el-input v-model="form.username"></el-input>
+            </el-form-item>
+            <el-form-item label="Password" prop="password">
+              <el-input v-model="form.password" show-password></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('form')">
+                Login
+              </el-button>
+            </el-form-item>
+          </el-form>
         </div>
       </div>
       <div class="flex">
@@ -30,3 +43,37 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+      },
+      rules: {
+        username: [
+          { required: true, message: 'This field is required', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: 'This field is required', trigger: 'blur' },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(form) {
+      console.log(form);
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          console.log('submit!');
+          return true;
+        }
+        console.log('error submit!!');
+        return false;
+      });
+    },
+  },
+};
+</script>
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import {
   Button,
+  Form,
+  FormItem,
+  Input,
 } from 'element-ui';
 import lang from 'element-ui/lib/locale/lang/en';
 import locale from 'element-ui/lib/locale';
@@ -8,3 +11,6 @@ import locale from 'element-ui/lib/locale';
 locale.use(lang);
 
 Vue.use(Button);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
