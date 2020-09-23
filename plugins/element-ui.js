7724b21c2b4664136b2722fee0efdc6709140d9b
yozzd <yozzdharma@gmail.com>
2020-09-23 11:15:53 +0700

    main css

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
-import Element from 'element-ui';
-import locale from 'element-ui/lib/locale/lang/en';
+import {
+  Button,
+} from 'element-ui';
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
 
-Vue.use(Element, { locale });
+locale.use(lang);
+
+Vue.use(Button);
