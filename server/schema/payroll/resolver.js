863854c84082c432b67a8425b25bb65b37f14c70
yozzd <yozzdharma@gmail.com>
2021-03-31 08:35:46 +0700

    payroll slip query

--- server/schema/payroll/resolver.js
+++ server/schema/payroll/resolver.js
@@ -445,7 +445,7 @@ const Query = {
       const payroll = await Payroll.aggregate([
         { $match: { _id: id } },
         { $unwind: '$employee' },
-        { $match: { 'employee.ex0': { $ne: true } } },
+        { $match: { 'employee.ex0': false } },
         {
           $group: {
             _id: '$_id',
