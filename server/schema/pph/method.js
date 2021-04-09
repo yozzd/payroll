e0c306ef3cbdede626261fd55e39576b239c74ac
yozzd <yozzdharma@gmail.com>
2021-04-09 15:30:17 +0700

    generate pdf pph: column1

--- server/schema/pph/method.js
+++ server/schema/pph/method.js
@@ -2,7 +2,7 @@ const { GraphQLError } = require('graphql');
 const PdfPrinter = require('pdfmake');
 const fs = require('fs-extra');
 
-const { intpre0, intpre0v2 } = require('../scalar/number');
+const { intpre0 } = require('../scalar/number');
 
 const fonts = {
   Roboto: {
@@ -15,39 +15,120 @@ const printer = new PdfPrinter(fonts);
 
 const genPDF = async (p) => {
   try {
-    const [e] = p.employee;
     await fs.ensureDir(`static/pph/${p.dir}`);
 
     const widths = [10, 150, 60];
 
     const col1 = [
-      [{ text: '', border: [true, true, false, false] }, { text: 'Nama Karyawan', border: [false, true, false, false] }, { text: e.d0, border: [false, true, true, false] }],
-      [{ text: '', border: [true, false, false, false] }, { text: 'Status Tanggungan', border: [false, false, false] }, { text: e.r0, border: [false, false, true, false] }],
-      [{ text: '', border: [true, false, false, false] }, { text: e.ea0, border: [false, false, false], alignment: 'right' }, { text: 'Bulanan', border: [false, false, true, false] }],
-      [{ text: '', border: [true, false, false, false] }, { text: 'Gaji Pokok', border: [false, false, false], bold: true }, {
-        text: intpre0(e.l0).format(), border: [false, false, true, false], alignment: 'right', bold: true,
+      [{
+        text: '', border: [true, true, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Nama Karyawan', border: [false, false, false, false] }, { text: p.d0, border: [false, false, true, false] }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Status Tanggungan', border: [false, false, false, false] }, { text: p.r0, border: [false, false, true, false] }],
+      [{ text: '', border: [true, false, false, false] }, { text: p.ea0, border: [false, false, false, false], alignment: 'right' }, { text: 'Bulanan', border: [false, false, true, false] }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Gaji Pokok', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.l0).format(), border: [false, false, true, false], alignment: 'right', bold: true,
       }],
-      [{ text: '1', border: [true, false, false, false], alignment: 'center' }, { text: 'Tunjangan Tetap & Tidak Tetap', border: [false, false, false] }, { text: intpre0(e.bk0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '2', border: [true, false, false, false], alignment: 'center' }, { text: 'Lembur & Insentif', border: [false, false, false] }, { text: intpre0(e.ai0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '3', border: [true, false, false, false], alignment: 'center' }, { text: 'Jaminan Kecelakaan Kerja, 0.54%', border: [false, false, false] }, { text: intpre0(e.cb0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '4', border: [true, false, false, false], alignment: 'center' }, { text: 'Jaminan Kematian, 0.30%', border: [false, false, false] }, { text: intpre0(e.cc0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '5', border: [true, false, false, false], alignment: 'center' }, { text: 'Iuran BPJS Kesehatan, 4% dibayar perusahaan', border: [false, false, false] }, { text: intpre0(e.cq0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '6', border: [true, false, false, false], alignment: 'center' }, { text: 'Retro Fill', border: [false, false, false] }, { text: intpre0(e.bu0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '7', border: [true, false, false, false], alignment: 'center' }, { text: 'Bonus & THR', border: [false, false, false] }, { text: intpre0(e.dr0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '8', border: [true, false, false, false], alignment: 'center' }, { text: 'Cuti yang diuangkan', border: [false, false, false] }, { text: intpre0(e.bz0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '9', border: [true, false, false, false], alignment: 'center' }, { text: '(Other Deduction)', border: [false, false, false] }, { text: intpre0(e.df0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '10', border: [true, false, false, false], alignment: 'center' }, { text: '(Absent Deduction)', border: [false, false, false] }, { text: intpre0(e.cy0).format(), border: [false, false, true, false], alignment: 'right' }],
-      [{ text: '', border: [true, false, false, false] }, { text: 'Total Penghasilan Bruto', border: [false, false, false], bold: true }, {
-        text: intpre0(e.bruto).format(), border: [false, false, true, false], alignment: 'right', bold: true,
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '1', border: [true, false, false, false], alignment: 'center' }, { text: 'Tunjangan Tetap & Tidak Tetap', border: [false, false, false, false] }, { text: intpre0(p.bk0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '2', border: [true, false, false, false], alignment: 'center' }, { text: 'Lembur & Insentif', border: [false, false, false, false] }, { text: intpre0(p.ai0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '3', border: [true, false, false, false], alignment: 'center' }, { text: 'Jaminan Kecelakaan Kerja, 0.54%', border: [false, false, false, false] }, { text: intpre0(p.cb0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '4', border: [true, false, false, false], alignment: 'center' }, { text: 'Jaminan Kematian, 0.30%', border: [false, false, false, false] }, { text: intpre0(p.cc0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '5', border: [true, false, false, false], alignment: 'center' }, { text: 'Iuran BPJS Kesehatan, 4% dibayar perusahaan', border: [false, false, false, false] }, { text: intpre0(p.cq0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '6', border: [true, false, false, false], alignment: 'center' }, { text: 'Retro Fill', border: [false, false, false, false] }, { text: intpre0(p.bu0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '7', border: [true, false, false, false], alignment: 'center' }, { text: 'Bonus & THR', border: [false, false, false, false] }, { text: intpre0(p.dr0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '8', border: [true, false, false, false], alignment: 'center' }, { text: 'Cuti yang diuangkan', border: [false, false, false, false] }, { text: intpre0(p.bz0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '9', border: [true, false, false, false], alignment: 'center' }, { text: '(Other Deduction)', border: [false, false, false, false] }, { text: intpre0(p.df0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '10', border: [true, false, false, false], alignment: 'center' }, { text: '(Absent Deduction)', border: [false, false, false, false] }, { text: intpre0(p.cy0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Total Penghasilan Bruto', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.t1a).format(), border: [false, true, true, false], alignment: 'right', bold: true,
       }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '1', border: [true, false, false, false], alignment: 'center' }, { text: 'Biaya Jabatan', border: [false, false, false, false] }, { text: intpre0(p.t2a).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '2', border: [true, false, false, false], alignment: 'center' }, { text: 'Iuran JHT, 2% dari upah dibayar karyawan', border: [false, false, false, false] }, { text: intpre0(p.ce0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '3', border: [true, false, false, false], alignment: 'center' }, { text: 'Iuran Pensiun, 1% dari upah dibayar karyawan', border: [false, false, false, false] }, { text: intpre0(p.cj0).format(), border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Total Pengurang', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.t3a).format(), border: [false, true, true, false], alignment: 'right', bold: true,
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Total Neto Sebulan', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.t4a).format(), border: [false, false, true, false], alignment: 'right', bold: true,
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Penghasilan Neto Setahun', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.t5a).format(), border: [false, false, true, false], alignment: 'right', bold: true,
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'PTKP', border: [false, false, false, false] }, {
+        text: intpre0(p.t6a).format(), border: [false, false, true, false], alignment: 'right',
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Penghasilan Kena Pajak Setahun', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.t7a).format(), border: [false, false, true, false], alignment: 'right', bold: true,
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'PKP Setahun', border: [false, false, false, false], bold: true }, {
+        text: intpre0(p.t8a).format(), border: [false, false, true, false], alignment: 'right', bold: true,
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Tarif Pph21', border: [false, false, false, false], bold: true }, {
+        text: 'Tarif', border: [false, false, true, false], bold: true,
+      }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Level 1 : 0 - 50 Juta', border: [false, false, false, false] }, { text: '5%', border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Level 2 : 50 - 250 Juta', border: [false, false, false, false] }, { text: '15%', border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Level 3 : 250 - 500 Juta', border: [false, false, false, false] }, { text: '25%', border: [false, false, true, false], alignment: 'right' }],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Level 4 : lebih dari 500 Juta', border: [false, false, false, false] }, { text: '30%', border: [false, false, true, false], alignment: 'right' }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Pph21 Tahunan', border: [false, false, false, false] }, {
+        text: intpre0(p.t9a).format(), border: [false, false, true, false], alignment: 'right',
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Pph21 Bulanan', border: [false, false, false, false] }, {
+        text: intpre0(p.t10a).format(), border: [false, false, true, false], alignment: 'right',
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Memiliki NPWP', border: [false, false, false, false] }, {
+        text: p.p0, border: [false, false, true, false], alignment: 'right',
+      }],
+      [{
+        text: '', border: [true, false, true, false], colSpan: 3,
+      }, '', ''],
+      [{ text: '', border: [true, false, false, false] }, { text: 'Pph21 Bulanan', border: [false, false, false, false] }, {
+        text: intpre0(p.t11a).format(), border: [false, false, true, false], alignment: 'right',
+      }],
+      [{
+        text: '', border: [true, false, true, true], colSpan: 3,
+      }, '', ''],
     ];
 
     const col2 = [
-      ['', 'Nama Karyawan', e.d0],
+      ['', 'Nama Karyawan', p.d0],
     ];
 
     const col3 = [
-      ['', 'Nama Karyawan', e.d0],
+      ['', 'Nama Karyawan', p.d0],
     ];
 
     const docDefinition = {
@@ -82,14 +163,14 @@ const genPDF = async (p) => {
       styles: {
         tbl1: {
           fontSize: 6,
-          margin: [-10, -10, -10, 0],
+          margin: [-20, -20, -20, 0],
         },
       },
     };
 
     return new Promise((resolve) => {
       const pdfDoc = printer.createPdfKitDocument(docDefinition);
-      pdfDoc.pipe(fs.createWriteStream(`static/pph/${p.dir}/${e.pph.name}.pdf`));
+      pdfDoc.pipe(fs.createWriteStream(`static/pph/${p.dir}/${p.pph.name}.pdf`));
       pdfDoc.on('end', () => {
         resolve({ sStatus: 1 });
       });
--- server/schema/pph/resolver.js
+++ server/schema/pph/resolver.js
@@ -53,33 +53,156 @@ const Mutation = {
         { $unwind: '$employee' },
         { $match: { 'employee._id': eId } },
         {
-          $group: {
+          $project: {
             _id: '$_id',
-            period: { $first: '$period' },
-            year: { $first: '$year' },
-            dir: { $first: '$dir' },
-            employee: {
-              $push: {
-                _id: '$employee._id',
-                d0: '$employee.d0',
-                e0: '$employee.e0',
-                l0: '$employee.l0',
-                r0: '$employee.r0',
-                ai0: '$employee.ai0',
-                bk0: '$employee.bk0',
-                bu0: '$employee.bu0',
-                bz0: '$employee.bz0',
-                cb0: '$employee.cb0',
-                cc0: '$employee.cc0',
-                cq0: '$employee.cq0',
-                cy0: '$employee.cy0',
-                df0: '$employee.df0',
-                dr0: '$employee.dr0',
-                ea0: '$employee.ea0',
-                pph: {
-                  name: '$employee.slip.name',
-                },
-                bruto: { $subtract: [{ $sum: ['$employee.l0', '$employee.bk0', '$employee.ai0', '$employee.cb0', '$employee.cc0', '$employee.cq0', '$employee.bu0', '$employee.dr0', '$employee.bz0'] }, { $sum: ['$employee.df0', '$employee.cy0'] }] },
+            dir: '$dir',
+            rate: '$rate',
+            eId: '$employee._id',
+            d0: '$employee.d0',
+            e0: '$employee.e0',
+            l0: '$employee.l0',
+            p0: '$employee.p0',
+            r0: '$employee.r0',
+            ai0: '$employee.ai0',
+            bk0: '$employee.bk0',
+            bu0: '$employee.bu0',
+            bz0: '$employee.bz0',
+            cb0: '$employee.cb0',
+            cc0: '$employee.cc0',
+            ce0: '$employee.ce0',
+            cj0: '$employee.cj0',
+            cq0: '$employee.cq0',
+            cy0: '$employee.cy0',
+            df0: '$employee.df0',
+            dr0: '$employee.dr0',
+            ea0: '$employee.ea0',
+            fc0: '$employee.fc0',
+            pph: {
+              name: '$employee.slip.name',
+            },
+          },
+        },
+        {
+          $addFields: {
+            t1a: { $subtract: [{ $sum: ['$l0', '$bk0', '$ai0', '$cb0', '$cc0', '$cq0', '$bu0', '$dr0', '$bz0'] }, { $sum: ['$df0', '$cy0'] }] },
+          },
+        },
+        {
+          $addFields: {
+            t2a: {
+              $function: {
+                body: `function(v) {
+                  return (v * 0.05 >= 500000) ? 500000 : Math.round(v * 0.05);
+                }`,
+                args: ['$t1a'],
+                lang: 'js',
+              },
+            },
+          },
+        },
+        {
+          $addFields: {
+            t3a: { $sum: ['$t2a', '$ce0', '$cj0'] },
+          },
+        },
+        {
+          $addFields: {
+            t4a: { $subtract: ['$t1a', '$t3a'] },
+          },
+        },
+        {
+          $addFields: {
+            t5a: { $multiply: ['$t4a', '$ea0'] },
+          },
+        },
+        {
+          $addFields: {
+            t6a: {
+              $function: {
+                body: `function(v, r) {
+                  const ptkpObject = {
+                    'TK/0': r.b4,
+                    'TK/1': r.b5,
+                    'TK/2': r.b6,
+                    'TK/3': r.b7,
+                    'K/0': r.b8,
+                    'K/1': r.b9,
+                    'K/2': r.b10,
+                    'K/3': r.b11,
+                  };
+                  return ptkpObject[v];
+                }`,
+                args: ['$r0', '$rate'],
+                lang: 'js',
+              },
+            },
+          },
+        },
+
+        {
+          $addFields: {
+            t7a: { $subtract: ['$t5a', '$t6a'] },
+          },
+        },
+
+        {
+          $addFields: {
+            t8a: {
+              $function: {
+                body: `function(v) {
+                  return (v <= 0 ? 0 : Math.floor(v / 1000) * 1000);
+                }`,
+                args: ['$t7a'],
+                lang: 'js',
+              },
+            },
+          },
+        },
+
+        {
+          $addFields: {
+            t9a: {
+              $function: {
+                body: `function(v) {
+                  const pph = Math.min(Math.max(0, v), 50000000) * 0.05 + Math.min(Math.max(0, v - 50000000), 200000000) * 0.15 + Math.min(Math.max(0, v - 250000000), 250000000) * 0.25 + Math.max(0, v - 500000000) * 0.3;
+                  return pph;
+                }`,
+                args: ['$t8a'],
+                lang: 'js',
+              },
+            },
+          },
+        },
+
+        {
+          $addFields: {
+            t10a: {
+              $function: {
+                body: `function(v, e) {
+                  return (v === 0 ? 0 : Math.round(v / e));
+                }`,
+                args: ['$t9a', '$ea0'],
+                lang: 'js',
+              },
+            },
+          },
+        },
+
+        {
+          $addFields: {
+            t11a: {
+              $function: {
+                body: `function(a, b, c) {
+                  if (a) {
+                    return a;
+                  } if (!a && b === 'Yes') {
+                    const v = Math.floor(c / 100) * 100;
+                    return (v <= 200 ? 0 : v);
+                  }
+                  return (Math.floor((pph21Bulanan * 1.2) / 100) * 100);
+                }`,
+                args: ['$fc0', '$p0', '$t10a'],
+                lang: 'js',
               },
             },
           },
