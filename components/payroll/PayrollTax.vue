916ad4a215b3831b2d35004b011d123f52ff84ab
yozzd <yozzdharma@gmail.com>
2021-04-21 10:00:09 +0700

    tax: guest auth

--- components/payroll/PayrollTax.vue
+++ components/payroll/PayrollTax.vue
@@ -33,7 +33,7 @@
       <el-table-column prop="d0" label="Nama Karyawan" width="300" fixed>
         <template slot-scope="scope">
           <el-link
-            v-if="!freeze"
+            v-if="!freeze && $auth.hasRole('user')"
             type="primary"
             class="font-sm"
             :underline="false"
