8420f3149c8c02af448786948af37f9a0a3c327d
yozz <yozzdharma@gmailcom>
2021-04-23 11:41:00 +0000

    earning others: pagination

--- components/payroll/PayrollEarningOthers.vue
+++ components/payroll/PayrollEarningOthers.vue
@@ -5,9 +5,7 @@
       :errors="errors"
     />
     <div class="flex space-x-4 items-center">
-      <div class="flex-1">
-        <span class="text-green-500">Total {{ items.length }} items</span>
-      </div>
+      <div class="flex-1"></div>
       <div class="w-64">
         <el-input
           v-model="search"
@@ -137,6 +135,18 @@
       </el-table-column>
       <el-table-column label="" min-width="120"></el-table-column>
     </el-table>
+    <el-pagination
+      :current-page.sync="page"
+      :page-sizes="pageSizes"
+      :page-size="pageSize"
+      :total="items.length"
+      :pager-count="pagerCount"
+      layout="total, sizes, prev, pager, next"
+      class="flex justify-end"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    >
+    </el-pagination>
 
     <el-dialog
       title="Edit Employee"
@@ -295,6 +305,7 @@ export default {
           this.items = employee;
           this.miniSearch.removeAll();
           this.miniSearch.addAll(this.items);
+          this.pageSizes.push(this.items.length);
         }
       },
       error({ graphQLErrors, networkError }) {
