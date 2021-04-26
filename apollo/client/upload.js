98facadac15cd6e63892f6d06341ef7bdbbbf266
yozz <yozzdharma@gmailcom>
2021-04-26 16:58:46 +0000

    uri

--- apollo/client/upload.js
+++ apollo/client/upload.js
@@ -2,5 +2,5 @@ import { createUploadLink } from 'apollo-upload-client';
 
 export default () => ({
   defaultHttpLink: false,
-  link: createUploadLink({ uri: 'http://190.1.7.100:3001/graphql' }),
+  link: createUploadLink({ uri: 'http://190.1.7.73:3001/graphql' }),
 });
