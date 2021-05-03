0c11c98a57ef17c0b6e0fc62db9155ae66d01c05
yozz <yozzdharma@gmailcom>
2021-05-03 08:32:27 +0700

    remove cache

--- apollo/client/default.js
+++
@@ -1,3 +0,0 @@
-export default () => ({
-  httpEndpoint: 'http://190.1.7.73:3001/graphql',
-});
--- apollo/client/upload.js
+++
@@ -1,6 +0,0 @@
-import { createUploadLink } from 'apollo-upload-client';
-
-export default () => ({
-  defaultHttpLink: false,
-  link: createUploadLink({ uri: 'http://190.1.7.73:3001/graphql' }),
-});
